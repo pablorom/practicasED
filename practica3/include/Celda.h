4e2171039e7144a9ac676c6d0e0e1f545ba6bfd7
pablorom <pablorom_11@hotmail.com>
2017-11-03 19:04:51 +0100

    Creo Pila_max_VD.cpp y su .h ademas de Celda.h

---
+++ practica3/include/Celda.h
@@ -0,0 +1,10 @@
+#ifndef Celda_h
+#define Celda_h
+
+struct Celda{
+    int dato;
+    int max;
+};
+
+
+#endif // Celda_h
---
+++ practica3/include/Pila_max_VD.h
@@ -0,0 +1,17 @@
+#ifndef PilaMaxVD_h
+#define PilaMaxVD_h
+
+
+#include "Vector_Dinamico.tpp"
+
+class PilaMaxVD{
+
+private:
+    Vector_Dinamico<celda> pila;
+    
+public:
+
+};
+
+
+#endif // PilaMaxVD_h
